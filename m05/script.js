03f0e24bdec4e3ce84b0f06fabe525fbaa693c59
Anton <143367976+t0nyTheQA@users.noreply.github.com>
2024-05-02 18:00:06 -0700

    Create script.js

---
+++ m05/script.js
@@ -0,0 +1,28 @@
+
+/*Ask for number/table*/
+var table = parseInt(prompt("Enter a number from 0 to 10:"));
+
+/*Verify number between 0 - 10 */
+if (table >= 0 && table <= 10) {
+    /* Initialize msg */
+    var msg = '<h2>Multiplication Table</h2>';
+
+//var operator = 'addition'; 
+
+    //create the multiplacation table using for loop to go thru 10 times
+    msg += '<table>';
+    for (var i = 1; i <= 10; i++) {
+         msg += i + ' x ' + table + ' = ' + (i * table) + '<br>';
+    }
+    msg += '</table>';
+
+    var el = document.getElementById('blackboard');
+    el.innerHTML = msg;
+} else {
+    /* Display an error message if the entered number is not 0-10 */
+    alert("Please enter a number from 0 to 10.");
+}
+
+
+
+
