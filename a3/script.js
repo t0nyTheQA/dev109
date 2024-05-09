42fff7c1ecc28caff3862fbdec049a7e919c53b5
Anton <143367976+t0nyTheQA@users.noreply.github.com>
2024-05-08 17:56:37 -0700

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,50 @@
 
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+    upDiamond(pHeight, pColorEven, pColorOdd, pSymbol);
+    downDiamond(pHeight, pColorEven, pColorOdd, pSymbol);
+}
+
+function upDiamond(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var rLine = "";
+    for (var i = 1; i <= pHeight; i++) {
+        var line = "";
+        var space = "&nbsp;";
+        for (var j = 1; j <= pHeight - i; j++) {
+            line += space;
+        }
+        for (var k = 0; k < 2 * i; k++) { 
+            var color;
+            if (k % 2 === 0) {
+                color = pColorEven;
+            } else {
+                color = pColorOdd;
+            }
+            line += "<span style='color:" + color + ";'>" + pSymbol + "</span>";
+        }
+        rLine += "<p>" + line + "</p>";
+    }
+    document.getElementById("diamond").innerHTML += rLine;
+}
+
+function downDiamond(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var rLine = "";
+    for (var i = 1; i <= pHeight - 1; i++) {
+        var line = "";
+        var space = "&nbsp;";
+        for (var j = 0; j < i; j++) {
+            line += space;
+        }
+        for (var k = (pHeight - i) * 2; k > 0; k--) { 
+            var color;
+            if (k % 2 === 0) {
+                color = pColorEven;
+            } else {
+                color = pColorOdd;
+            }
+            line += "<span style='color:" + color + ";'>" + pSymbol + "</span>";
+        }
+        rLine += "<p>" + line + "</p>";
+    }
+    document.getElementById("diamond").innerHTML += rLine;
+}
+
